98500286079207a3d640f2910a3c29ff6b631e48
Janka K <janakorycanova@gmail.com>
2022-03-22 18:50:24 +0100

    Feature main functionality of Sportka implemented

--- sportka.js
+++ sportka.js
@@ -1,8 +1,31 @@
 // Udělej losování klasické Sportky
-// Máš osudí čísel od 1 do 48
-// Náhodně z tohoto osudí vyber 7 čísel
+
+
 // Žádné číslo se nesmí v tahu opakovat
 // (je vyjmuté z osudí, takže už ho nemůžeš znovu vylosovat)
 // Z každého vylosovaného čísla vygeneruj následující HTML kód:
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
+
+// Máš osudí čísel od 1 do 48
+
+let numbers = [];
+
+for (let i = 1; i <= 48; i = i +1){
+    numbers.push(i);
+}
+
+// Náhodně z tohoto osudí vyber 7 čísel
+
+let randomNumbers = [];
+
+for (let i = 0; i < 7; i++){
+    let winnerIndex = Math.floor(Math.random() * numbers.length);
+    let winnerNumber = numbers[winnerIndex]; 
+
+    randomNumbers.push(winnerNumber);
+    numbers.splice(winnerIndex,1);
+}
+
+
+
